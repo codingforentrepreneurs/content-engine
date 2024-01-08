710acc3e06cb710dfd55c13fd981735538fd71a0
CodingForEntrepreneurs <hello@teamcfe.com>
2024-01-08 12:58:44 -0700

    Homepage test

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from . import views
 urlpatterns = [
+    path("", views.home_page),
     path("admin/", admin.site.urls),
 ]
---
+++ src/cfehome/views.py
@@ -0,0 +1,4 @@
+from django.http import HttpResponse
+
+def home_page(request):
+    return HttpResponse("Hello world")
