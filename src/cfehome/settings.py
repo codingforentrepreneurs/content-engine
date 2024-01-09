63391571505eaa8b4a91d08649160f8dc02eed15
CodingForEntrepreneurs <hello@teamcfe.com>
2024-01-08 17:14:40 -0700

    8 - Your First Running Container on Kubernetes

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -9,13 +9,13 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+# import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+# print(os.environ.get("WELCOME_MSG"))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
