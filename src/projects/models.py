041edaa17e6d211c408e04cfd168c115592a2c1a
CodingForEntrepreneurs <hello@teamcfe.com>
2024-01-10 13:58:21 -0700

    31 - Override Save to Generate Slug Handle

--- src/projects/models.py
+++ src/projects/models.py
@@ -1,12 +1,18 @@
 from django.conf import settings
 from django.db import models
+from django.utils.text import slugify
 
 User = settings.AUTH_USER_MODEL
 
 class Project(models.Model):
     owner = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
-    title = models.CharField(max_length=120, null=True, blank=True)
+    title = models.CharField(max_length=120, null=True)
     handle = models.SlugField(null=True, blank=True)
     active = models.BooleanField(default=True)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def save(self, *args, **kwargs):
+        if not self.handle:
+            self.handle = slugify(self.title)
+        super().save(*args, **kwargs)
