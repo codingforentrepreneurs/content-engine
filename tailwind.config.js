4965c55bd95f55ec4217cf85d415db9f03ed3683
CodingForEntrepreneurs <hello@teamcfe.com>
2024-01-10 12:06:04 -0700

    Added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,62 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: ["./src/templates/**/*.{html,js}"],
+  theme: {
+    colors: {
+      primary: {
+        "50":"#eff6ff",
+        "100":"#243b6d",
+        "200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"},
+      white: colors.white,
+      black: colors.black,
+      cfe: "#007cae",
+      cfeBlue: {
+        100: "#007cad"
+      },
+      stone: colors.stone,
+      sky: colors.sky,
+      violet: colors.violet,
+    },
+    fontFamily: {
+      'body': [
+    'Inter', 
+    'ui-sans-serif', 
+    'system-ui', 
+    '-apple-system', 
+    'system-ui', 
+    'Segoe UI', 
+    'Roboto', 
+    'Helvetica Neue', 
+    'Arial', 
+    'Noto Sans', 
+    'sans-serif', 
+    'Apple Color Emoji', 
+    'Segoe UI Emoji', 
+    'Segoe UI Symbol', 
+    'Noto Color Emoji'
+  ],
+      'sans': [
+    'Inter', 
+    'ui-sans-serif', 
+    'system-ui', 
+    '-apple-system', 
+    'system-ui', 
+    'Segoe UI', 
+    'Roboto', 
+    'Helvetica Neue', 
+    'Arial', 
+    'Noto Sans', 
+    'sans-serif', 
+    'Apple Color Emoji', 
+    'Segoe UI Emoji', 
+    'Segoe UI Symbol', 
+    'Noto Color Emoji'
+    ]
+    },
+    extend: {},
+  },
+  
+  plugins: [],
+}
