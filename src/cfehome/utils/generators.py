bee1e8745e418f664cb7cc3109f30a0262927dae
CodingForEntrepreneurs <hello@teamcfe.com>
2024-01-10 14:16:30 -0700

    32 - Unique Slugify For SlugFields

---
+++ src/cfehome/utils/generators.py
@@ -0,0 +1,32 @@
+import random
+import string
+from django.utils.text import slugify
+
+
+def generate_random_string(size=10):
+    chars = string.ascii_lowercase + string.digits
+    random_chars = [random.choice(chars) for _ in range(0, size)]
+    return "".join(random_chars)
+
+
+def unique_slugify(instance, _slug=None, size=5, slug_field="slug", title_field='title'):
+    '''
+    celery tasks
+    '''
+    to_slug_field = getattr(instance, title_field)
+    slug = slugify(to_slug_field)
+    if _slug is not None:
+        slug = slugify(_slug)
+    lookup = {}
+    lookup[f"{slug_field}__iexact"] =  slug
+    ModelClass = instance.__class__
+    qs_exists = ModelClass.objects.filter(**lookup).exists()
+    if qs_exists:
+        """
+        Not unique slug
+        generate one
+        """
+        random_str = generate_random_string(size=size)
+        new_slug = slugify(f"{to_slug_field} {random_str}")
+        return unique_slugify(instance, _slug=new_slug, size=size, slug_field=slug_field, title_field=title_field)
+    return slug
--- src/projects/models.py
+++ src/projects/models.py
@@ -1,18 +1,19 @@
 from django.conf import settings
 from django.db import models
 from django.utils.text import slugify
+from cfehome.utils.generators import unique_slugify
 
 User = settings.AUTH_USER_MODEL
 
 class Project(models.Model):
     owner = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=120, null=True)
-    handle = models.SlugField(null=True, blank=True)
+    handle = models.SlugField(null=True, blank=True, unique=True)
     active = models.BooleanField(default=True)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
 
     def save(self, *args, **kwargs):
         if not self.handle:
-            self.handle = slugify(self.title)
+            self.handle = unique_slugify(self, slug_field='handle')
         super().save(*args, **kwargs)
