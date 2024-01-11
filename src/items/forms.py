824a0ef8c1f561c11aa5fb90d32e17756448827d
CodingForEntrepreneurs <hello@teamcfe.com>
2024-01-11 11:47:13 -0700

    Styling Django Forms the TailwindCSS way

--- src/items/forms.py
+++ src/items/forms.py
@@ -7,3 +7,9 @@ class ItemForm(forms.ModelForm):
     class Meta:
         model = Item
         fields = ['title', 'description']
+
+    def clean_title(self):
+        raise forms.ValidationError("there was somthing wrong")
+
+    def clean_description(self):
+        raise forms.ValidationError("there was somthing wrong")
