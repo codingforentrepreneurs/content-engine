789680282f3c9f41fd57ffbd0af32e24db5b43ce
CodingForEntrepreneurs <hello@teamcfe.com>
2024-01-12 12:31:51 -0700

    53 - Integrating HTMX with TailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 const colors = require('tailwindcss/colors')
+const plugin = require('tailwindcss/plugin');
 
 module.exports = {
   content: [
@@ -65,5 +66,14 @@ module.exports = {
     extend: {},
   },
   
-  plugins: [require('flowbite/plugin')],
+  plugins: [
+    require('flowbite/plugin'),
+    plugin(function({ addVariant }) {
+      addVariant('htmx', ({ modifySelectors, separator }) => {
+        modifySelectors(({ className }) => {
+          return `.htmx-request .htmx\\:${className}`;
+        });
+      });
+    }),
+  ],
 }
