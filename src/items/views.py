294d5557028e2bd18cbbcc52fa8ef27c00885154
CodingForEntrepreneurs <hello@teamcfe.com>
2024-01-12 11:06:10 -0700

    50 - HTMX for Rendering Django Forms

--- src/items/views.py
+++ src/items/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render, get_object_or_404
-
+from django.urls import reverse 
 from projects import cache as projects_cache
+from django_htmx.http import HttpResponseClientRefresh
 from projects.decorators import project_required
 
 from . import forms
@@ -45,14 +46,23 @@ def item_delete_view(request, id=None):
 @project_required
 @login_required
 def item_create_view(request):
+    template_name = 'items/create.html'
+    if request.htmx:
+        template_name = 'items/snippets/form.html'
     form = forms.ItemCreateForm(request.POST or None)
     if form.is_valid():
         item_obj = form.save(commit=False)
         item_obj.project = request.project
         item_obj.added_by = request.user 
         item_obj.save()
+        if request.htmx:
+            print("is htmx")
+            return HttpResponseClientRefresh()
         return redirect(item_obj.get_absolute_url())
+    action_create_url = reverse("items:create")
     context = {
-        "form": form
+        "form": form,
+        "btn_label": "Create item",
+        "action_url": action_create_url
     }
-    return render(request, 'items/create.html', context)
+    return render(request, template_name, context)
